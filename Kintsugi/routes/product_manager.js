f120d22db758444eac84e8583c1bf2f5a14084c2
AngelesRodriguezSchon <angeles.rodriguezschon@gmail.com>
2020-11-29 12:06:51 -0300

    file uploads de productos

--- Kintsugi/routes/product_manager.js
+++ Kintsugi/routes/product_manager.js
@@ -1,11 +1,24 @@
 var express = require('express');
 const productController = require('../controllers/productController');
 var router = express.Router();
+let multer = require('multer');
+let path = require ('path');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '../tmp/my-uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+  })
+   
+  var upload = multer({ storage: storage })
 
 router.get('/', productController.renderProductManager);
 
 router.get('/add', productController.renderProductAdd);
-router.post('/store', productController.store);
+router.post('/store', upload.any(), productController.store);
 
 router.get('/edit/:id?', productController.renderProductEdit);
 router.post("/edit/:id", productController.update);
