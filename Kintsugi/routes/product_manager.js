b8dc65bc5129cc0769a208730235d06061c39986
AngelesRodriguezSchon <angeles.rodriguezschon@gmail.com>
2021-03-12 11:50:58 -0300

    authmiddlewares en rutas de pm

--- Kintsugi/routes/product_manager.js
+++ Kintsugi/routes/product_manager.js
@@ -3,6 +3,7 @@ const productController = require('../controllers/productController');
 var router = express.Router();
 let multer = require('multer');
 let path = require ('path');
+const authMiddleware = require('../middlewares/authMiddleware');
 const pmMiddleware = require('../middlewares/pmMiddleware');
 const {check, validationResult, body} = require('express-validator');
 
@@ -17,15 +18,15 @@ var storage = multer.diskStorage({
    
   var upload = multer({ storage: storage })
 
-router.get('/', pmMiddleware,  productController.renderProductManager);
+  router.get('/', authMiddleware, pmMiddleware,  productController.renderProductManager);
 
-router.get('/add', pmMiddleware, productController.renderProductAdd);
+router.get('/add', authMiddleware, pmMiddleware, productController.renderProductAdd);
 router.post('/store', upload.any(), [check('description').isLength({max:200}), check('price').isInt()], productController.store);
 
-router.get('/edit/:id?', pmMiddleware, productController.renderProductEdit);
+router.get('/edit/:id?', authMiddleware, pmMiddleware, productController.renderProductEdit)
 router.post('/edit/:id', upload.any(), [check('description').isLength({max:200}), check('price').isInt()], productController.update);
 
-router.get('/destroy/:id', pmMiddleware, productController.destroy);
+router.get('/destroy/:id', authMiddleware, pmMiddleware, productController.destroy);
 
 
 
